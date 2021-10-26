130a81cf778014d454fe3a7310abd31fcdbbd6b4
Alen <alenaju1304@gmail.com>
2021-10-26 17:54:07 +0400

    final update to logic

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ function computerPlay() {
   let computer = arr[randomVariable];
   return computer;
 }
+
 function userPlay() {
   let arr = ["rock", "paper", "scissor"];
   let randomVariable = Math.floor(Math.random() * arr.length);
@@ -21,42 +22,39 @@ function game(userSelection, computerSelection) {
     (userSelection === "scissor" && computerSelection === "rock") ||
     (userSelection === "paper" && computerSelection === "scissor")
   ) {
-    return 1;
-    // return `${computerSelection} beats ${userSelection}, you loose! `;
+    return 0;
   } else if (
     (userSelection === "rock" && computerSelection === "scissor") ||
     (userSelection === "scissor" && computerSelection === "paper") ||
     (userSelection === "paper" && computerSelection === "rock")
   ) {
     return 1;
-    // return `${userSelection} beats ${computerSelection}, you win! `;
   } else if (
     (userSelection === "rock" && computerSelection === "rock") ||
     (userSelection === "scissor" && computerSelection === "scissor") ||
     (userSelection === "paper" && computerSelection === "paper")
   ) {
-    return 0;
-    // return `its a draw! `;
+    return 2;
   } else return `invalid option`;
 }
-// console.log(game(userPlay(), computerPlay()));
 
-// let result = game("rock");
-// console.log(game("rocK".toLowerCase(), computerPlay()));
 function final(noOfTimes) {
   let a = 0;
   let b = 0;
   for (let i = 0; i < noOfTimes; i++) {
-    if (game(userPlay(), computerPlay()) === 1) {
+    let user = userPlay();
+    let comp = computerPlay();
+    if (game(user, comp) === 0) {
       a = a + 1;
-    } else if (game(userPlay(), computerPlay()) === 2) {
+    } else if (game(user, comp) === 1) {
       b = b + 1;
-    } else if (game(userPlay(), computerPlay()) === 0) {
+    } else if (game(user, comp) === 2) {
       a = a + 1;
+
       b = b + 1;
-    }
+    } else return "wrong input";
   }
-  console.log(a);
-  console.log(b);
+  console.log("final" + a);
+  console.log("fianl" + b);
 }
 console.log(final(5));
